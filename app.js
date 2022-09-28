168b8758e435ef21e38e95b306438cf2e7c4ebdb
Denis <kuuushnirrr@gmail.com>
2022-09-28 21:28:14 +0300

    add start page

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile("static/index.html");
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+  console.log(`Example app listening on port ${PORT}`);
 })
